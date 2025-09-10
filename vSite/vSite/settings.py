9cb257d3d6fdda998b659f70b99ef5d013ac6ab1
Sega <sfoxig@ya.ru>
2025-09-11 02:41:15 +0300

    fix

--- vSite/vSite/settings.py
+++ vSite/vSite/settings.py
@@ -24,6 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+if SECRET_KEY:
+    pass
+else:
+    SECRET_KEY = 'xcdfvdsgsdfgrfgfsgfgdfgfbxcvfserfeg'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
