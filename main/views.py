543bd6590552f62d96866c2315b7ee4a3b580a92
Sega <sfoxig@ya.ru>
2025-09-23 02:58:58 +0300

    fix

--- main/views.py
+++ main/views.py
@@ -1,11 +1,13 @@
 from django.views.generic import TemplateView
 from search.utils import cities, get_or_session
+from vSite.settings import DEBUG
 
 
 class IndexView(TemplateView):
     template_name = 'main/main.html'
 
     def get_context_data(self, **kwargs):
+        print(DEBUG)
         context = super().get_context_data(**kwargs)
         context["looking_for"] = get_or_session(self, 'looking_for')
         context["goal"] = get_or_session(self, 'goal')
