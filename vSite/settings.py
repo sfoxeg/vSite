ad346390a4ab438b3e985103835ee42256805642
Sega <sfoxig@ya.ru>
2025-09-23 01:37:33 +0300

    fix

--- vSite/settings.py
+++ vSite/settings.py
@@ -148,4 +148,4 @@ AUTHENTICATION_BACKENDS = [
     'user.authentication.EmailAuthBackend',
 ]
 
-CSRF_TRUSTED_ORIGINS = ['http://192.168.31.221:8080/']
+CSRF_TRUSTED_ORIGINS = ['http://mrsega.duckdns.org:8777', 'http://192.168.31.221:8777']
