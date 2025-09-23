735670cb64ce81e5f0776eed70ae082c1dc3b7b9
Sega <sfoxig@ya.ru>
2025-09-23 03:45:14 +0300

    fix

--- vSite/settings.py
+++ vSite/settings.py
@@ -25,6 +25,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
+DEBUG = False if DEBUG == 'False' else True
+
 ALLOWED_HOSTS = ['*']
 
 # Application definition
